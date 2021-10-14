dd2c0e9b36ebdeead56c72175c16e9acfb763e3f
Margo Bobyleva <shinebebi@yandex.ru>
2021-10-15 01:04:39 +0300

    api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,22 +5,48 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import appStyles from "./app.module.css"
 
-import data from '../../utils/data'
+function App () {
 
-class App extends React.Component {
-    state = {
-        ingredientsList: data
+    const urlIngredients = 'https://norma.nomoreparties.space/api/ingredients'
+
+
+    const [state, setState] = React.useState({
+        isLoading: false,
+        hasError: false,
+        data: []
+    })
+
+    const getIngredients = () => {
+        setState({ ...state, hasError: false, isLoading: true });
+        fetch(urlIngredients)
+            .then(res => res.json())
+            .then(data => setState({ ...state, data: data.data, isLoading: false }))
+            .catch(e => {
+                setState({ ...state, hasError: true, isLoading: false });
+            });
     }
-    render() {
-        return (
-            <>
-                <AppHeader/>
-                <div className={appStyles.main}>
-                    <BurgerIngredients buns={this.state.ingredientsList.filter(e => e.type === "bun")} mains={this.state.ingredientsList.filter(e => e.type === "main")} sauces={this.state.ingredientsList.filter(e => e.type === "sauce")}/>
-                    <BurgerConstructor points={this.state.ingredientsList}/>
-                </div>
-            </>
-        );
-    }
+
+    React.useEffect (() => {
+        getIngredients()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const { data, isLoading, hasError } = state;
+    return (
+        <div>
+            <AppHeader/>
+            <div className={appStyles.main}>
+                {isLoading && 'Загрузка...'}
+                {hasError && 'Произошла ошибка'}
+                {!isLoading && !hasError && data.length &&
+                <BurgerIngredients
+                    buns={data.filter(e => e.type === "bun")}
+                    mains={data.filter(e => e.type === "main")}
+                    sauces={data.filter(e => e.type === "sauce")}/>
+                    && <BurgerConstructor points={data}/>
+                }
+            </div>
+        </div>
+    );
 }
 export default App
