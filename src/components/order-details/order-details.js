b8de81044027f87b9fb491fd392a119f60910596
Margo Bobyleva <shinebebi@yandex.ru>
2021-11-05 11:02:55 +0300

    propTypes

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -18,7 +18,7 @@ function OrderDetails () {
                 <div className={OrderDetailsStyles.infoOrder__container}>
                     <p className={`${OrderDetailsStyles.order_number} text text_type_digits-large`}>{orderNumber}</p>
                     <p className={`${OrderDetailsStyles.id_description} text text_type_main-default`}>идентификатор заказа</p>
-                    <img src={done} className={OrderDetailsStyles.done_img}/>
+                    <img src={done} className={OrderDetailsStyles.done_img} alt='done img'/>
                     <p className={`${OrderDetailsStyles.order_startCooking} text text_type_main-default`}>Ваш заказ начали готовить</p>
                     <p className={`${OrderDetailsStyles.order_instruction} text text_type_main-default text_color_inactive`}>Дождитесь готовности на орбитальной станции</p>
                 </div>
